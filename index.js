cde52bc75630bedab4e9bfe9bf22facf59cb4aa8
Valts Vilcans <valts03@inbox.lv>
2020-07-04 16:32:39 +0200

    [rendering templates example]

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const path = require("path");
 const logger = require("./middleware/logger");
+const exphbs = require("express-handlebars");
+const members = require("./members");
 
 const app = express();
 
 // init middleware
 // app.use(logger);
 
+// handlebars middleware
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// homepage route
+
+app.get("/", (req, res) =>
+  res.render("index", { title: "Member app title", members })
+);
 
 // set static folder
 
