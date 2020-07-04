96a1007ca18619bfdda21b25f180fa954bfda552
Valts Vilcans <valts03@inbox.lv>
2020-07-04 15:41:33 +0200

    [express delete example]

--- routes/api/members.js
+++ routes/api/members.js
@@ -57,4 +57,19 @@ router.put("/:id", (req, res) => {
   }
 });
 
+router.delete("/:id", (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json({
+      msg: "Member deleted",
+      members: members.filter(
+        (member) => member.id !== parseInt(req.params.id)
+      ),
+    });
+  } else {
+    res.status(400).json({ msg: "Member not found" });
+  }
+});
+
 module.exports = router;
