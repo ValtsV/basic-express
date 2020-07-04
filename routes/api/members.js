c5946dfaf811487dae05c618b1cfc050d40820d1
Valts Vilcans <valts03@inbox.lv>
2020-07-04 15:36:40 +0200

    [express put example]

--- routes/api/members.js
+++ routes/api/members.js
@@ -37,4 +37,24 @@ router.post("/", (req, res) => {
   res.json(members);
 });
 
+// update
+
+router.put("/:id", (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+
+  if (found) {
+    const updatedMember = req.body;
+    members.forEach((member) => {
+      if (member.id === parseInt(req.params.id)) {
+        member.name = updatedMember.name ? updatedMember.name : member.name;
+        member.email = updatedMember.email ? updatedMember.email : member.email;
+
+        res.json({ msg: "Member updated", member });
+      }
+    });
+  } else {
+    res.status(400).json({ msg: "Member not found" });
+  }
+});
+
 module.exports = router;
